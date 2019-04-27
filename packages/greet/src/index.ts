c5d9e739d4a9edb3c23e0ee0140e62f8aed8d1d5
mytee306 <mateja176@gmail.com>
2019-04-27 13:43:13 +0200

    Reverts  to mapping to '*'

--- packages/greet/src/index.ts
+++ packages/greet/src/index.ts
@@ -1,4 +1,4 @@
-import { capitalize } from 'common';
+import { capitalize } from 'common/src';
 import getLast from 'common/src/getLast';
 
 export const greet = (name: string) => `Hello ${capitalize(name)}`;
