9f53170accee750fbd29d5da4c989bebd5a696de
mytee306 <mateja176@gmail.com>
2019-04-30 15:49:29 +0200

    Uses exec instead of spawn

--- run-scope.ts
+++ run-scope.ts
@@ -12,7 +12,7 @@ const {
     prefix = 'packages',
     _: [scriptName],
     $0,
-    ...additionalArgs
+    ...argsObject
   },
 } = yargs;
 
@@ -20,7 +20,9 @@ if (!scope) {
   throw new Error('Please provide a scope argument i.e. --scope myPackage');
 }
 
-const packageJSON = fs.readFileSync(path.join(__dirname, 'package.json'), {
+const packageJSONPath = path.join(__dirname, 'package.json');
+
+const packageJSON = fs.readFileSync(packageJSONPath, {
   encoding: 'UTF-8',
 });
 
@@ -29,41 +31,27 @@ const { scripts } = JSON.parse(packageJSON);
 const script = scripts[scriptName];
 
 if (!script) {
-  throw new Error(`'${scriptName}' not found in package.json`);
+  throw new Error(`'${scriptName}' not found in ${packageJSONPath}`);
 }
-
-const [command, ...args] = script.split(' ');
-
-const allArgs = args.concat(Object.entries(additionalArgs));
 
 const packagePath = path.join(__dirname, prefix as string, scope as string);
 
-const envKeys = Object.keys(process.env);
+const argsString = (Object.entries(argsObject) as string[][])
+  .reduce((args, argEntry) => args.concat(argEntry), [])
+  .join(' ');
 
-const env = Object.entries(process.env)
-  .map(([key, value]) => [
-    key,
-    value.includes(__dirname) ? value.replace(__dirname, packagePath) : value,
-  ])
-  .reduce<NodeJS.ProcessEnv>(
-    (tempEnv, [key, value]) => ({ ...tempEnv, [key]: value }),
-    {},
-  );
+const scriptWithAdditionalArgs = argsString
+  ? script.concat(' ', argsString)
+  : script;
 
-const argsWithEnv = allArgs.map((arg: string) => {
-  const argWithout$ = arg.slice(1);
-
-  return envKeys.includes(argWithout$) ? env[argWithout$] : arg;
+const childProcess = cp.exec(scriptWithAdditionalArgs, {
+  cwd: packagePath,
 });
 
-const spawnedCommand = cp.spawn(command, argsWithEnv, {
-  cwd: path.join(packagePath),
-});
+childProcess.stdout.on('data', data => console.log(chalk.italic(data)));
 
-spawnedCommand.stdout.on('data', data => console.log(chalk.italic(data)));
+childProcess.stderr.on('data', data => console.error(chalk.red(data)));
 
-spawnedCommand.stderr.on('data', data => console.error(chalk.red(data)));
-
-spawnedCommand.on('close', () =>
-  console.info(chalk.green(`✔ Command: '${command}', Args: '${args}'`)),
+childProcess.on('close', () =>
+  console.info(chalk.green(`✔ Script: '${scriptWithAdditionalArgs}'`)),
 );
